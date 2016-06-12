80ba838e26d23a235d39cb43a0887c19499a0edf
Ivan Garcia <ifgs1@hotmail.com>
2016-06-12 23:17:13 +0000

    removed array

--- client-test.js
+++ client-test.js
@@ -1,8 +1,6 @@
 var seneca = require('seneca')()
 var client = seneca.client({ host:'localhost', port:8080 })
 
-
-var arr = [1,2,3,4,5,6];
 // Envia al topico de la suma los operandos y espera respuesta
 client.act({ type: 'stock', function: 'stockByCategory'}, function (err, result) {
   console.log(result);
